322a6b2759632c002f1a0ec42204d84210cf9dc5
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-02 11:24:10 +0200

    Parser checks if file is a Bin or Ascii type

--- src/parserStl.cpp
+++ src/parserStl.cpp
@@ -1,14 +1,30 @@
 #include <fstream>
 #include "parserStl.h"
 #include <cstdint>
+#include <cstring>
 
 std::vector<TriangleStl>* Parser::parseFile(char input[], uint64_t size) {
     auto output = new std::vector<TriangleStl>();
 //TODO read first 5 bytes as char check if they are equal to solid if they are this is a ascii type stl
 //  Other wise it's binary and omit first 80 bytes
+    uint64_t  index = 0;
+    auto reference = "solid";
+    bool iftext = strncmp(input, reference, strlen(reference));
+
+    if(iftext)
+        else
 
 
     return output;
 }
+
+std::vector<TriangleStl> *Parser::parseAscii(char *input, uint64_t size) {
+    return nullptr;
+}
+
+std::vector<TriangleStl> *Parser::parseBin(char *input, uint64_t size) {
+    return nullptr;
+}
+
 //TODO Create parser of binary files
 //TODO Create parser of ASCII files
--- src/parserStl.h
+++ src/parserStl.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <fstream>
 class Parser{
+private:
+    std::vector<TriangleStl>* parseBin( char input[], uint64_t  size);
+    std::vector<TriangleStl>* parseAscii( char input[], uint64_t  size);
 public:
     static std::vector<TriangleStl>* parseFile( char input[], uint64_t  size);
 
