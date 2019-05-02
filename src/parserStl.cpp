bd5f0a9d466f01df58f01c64881b21185f9ff2ca
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-02 15:31:49 +0200

    Adding method which returns a length of non white space string of characters

--- src/parserStl.cpp
+++ src/parserStl.cpp
@@ -5,19 +5,8 @@
 //TODO consider adding class for error checking
 //TODO add strict error checking
 //TODO check if file is of a right size
-//TODO check if
 
 
-//TODO consider just incrementing pointer to speed up
-//Returns the pointer to place after the white space what ever this may be
-char* Parser::omitWhiteSpaces(char * input, uint64_t size){
-    uint64_t index = 0;
-    int ifWhiteSpace;
-    while (index < size(ifWhiteSpace = isspace((int)input[index]))){
-        index++;
-    }
-    return input+index;
-}
 std::vector<TriangleStl>* Parser::parseFile(char input[], uint64_t size) {
     auto output = new std::vector<TriangleStl>();
     uint64_t  index = 0;
@@ -30,13 +19,30 @@ std::vector<TriangleStl>* Parser::parseFile(char input[], uint64_t size) {
 }
 //TODO Create parser of ASCII files
 //TODO Check if keywords are strictly correct
-//input point's to a place after the header
 std::vector<TriangleStl> *Parser::parseAscii(char *input, uint64_t size) {
-    input = omitWhiteSpaces(input, size);
+    uint64_t afterWhiteSpace = omitWhiteSpaces(input, size);
+
+
+
     return nullptr;
 }
 //TODO Create parser of binary files
-//input point's to a place after the header
 std::vector<TriangleStl> *Parser::parseBin(char *input, uint64_t size) {
     return nullptr;
+}
+//TODO consider just incrementing pointer to speed up
+uint64_t Parser::omitWhiteSpaces(char * input, uint64_t limit){
+    uint64_t index = 0;
+    while (index < limit && (isspace((int)input[index])) != 0 ){
+        index++;
+    }
+    return index;
+}
+//TODO consider just incrementing pointer to speed up
+uint64_t Parser::shift(char *input, uint64_t limit) {
+    uint64_t index = 0;
+    while (index < limit && (isspace((int)input[index])) == 0 ){
+        index++;
+    }
+    return index;
 }
--- src/parserStl.h
+++ src/parserStl.h
@@ -5,8 +5,14 @@
 #include <fstream>
 class Parser{
 private:
-    static char* omitWhiteSpaces(char * input, uint64_t size);
+    //Returns length of data stream ahead
+    static uint64_t shift(char *input, uint64_t limit);
+    //Returns offset to place after the white space what ever this may be
+    //limit indicates how many bytes can be omitted at max
+    static uint64_t omitWhiteSpaces(char * input, uint64_t limit);
+    //input point's to a place after the header
     static std::vector<TriangleStl>* parseBin( char input[], uint64_t  size);
+    //input point's to a place after the header
     static std::vector<TriangleStl>* parseAscii( char input[], uint64_t  size);
 public:
     static std::vector<TriangleStl>* parseFile( char input[], uint64_t  size);
