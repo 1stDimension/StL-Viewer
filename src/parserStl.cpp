401143d9fc0cd9e626f05e00632d866590e1a241
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-02 10:56:26 +0200

    Parser no return pointer to a vector not a vector

--- src/parserStl.cpp
+++ src/parserStl.cpp
@@ -2,7 +2,7 @@
 #include "parserStl.h"
 #include <cstdint>
 
-std::vector<TriangleStl> Parser::parseFile(char input[], uint64_t size) {
+std::vector<TriangleStl>* Parser::parseFile(char input[], uint64_t size) {
     auto output = new std::vector<TriangleStl>();
 //TODO read first 5 bytes as char check if they are equal to solid if they are this is a ascii type stl
 //  Other wise it's binary and omit first 80 bytes
--- src/parserStl.h
+++ src/parserStl.h
@@ -5,6 +5,6 @@
 #include <fstream>
 class Parser{
 public:
-    static std::vector<TriangleStl> parseFile( char input[], uint64_t  size);
+    static std::vector<TriangleStl>* parseFile( char input[], uint64_t  size);
 
 };
