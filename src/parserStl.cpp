d09eec6cb622c7aee82156d0918c497b66629859
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-02 12:06:04 +0200

    Adding function which omits white space characters possible problem with stepping into restricted zone of memory

--- src/parserStl.cpp
+++ src/parserStl.cpp
@@ -2,29 +2,41 @@
 #include "parserStl.h"
 #include <cstdint>
 #include <cstring>
+//TODO consider adding class for error checking
+//TODO add strict error checking
+//TODO check if file is of a right size
+//TODO check if
 
+
+//TODO consider just incrementing pointer to speed up
+//Returns the pointer to place after the white space what ever this may be
+char* Parser::omitWhiteSpaces(char * input, uint64_t size){
+    uint64_t index = 0;
+    int ifWhiteSpace;
+    while (index < size(ifWhiteSpace = isspace((int)input[index]))){
+        index++;
+    }
+    return input+index;
+}
 std::vector<TriangleStl>* Parser::parseFile(char input[], uint64_t size) {
     auto output = new std::vector<TriangleStl>();
-//TODO read first 5 bytes as char check if they are equal to solid if they are this is a ascii type stl
-//  Other wise it's binary and omit first 80 bytes
     uint64_t  index = 0;
     auto reference = "solid";
-    bool iftext = strncmp(input, reference, strlen(reference));
-
-    if(iftext)
-        else
-
-
-    return output;
+    bool ifText = strncmp(input, reference, strlen(reference));
+    if(ifText)
+        return parseAscii(input + strlen(reference), size - strlen(reference));
+    else
+        return parseBin(input + 80, size - 80); //skip the 80 bytes long header
 }
-
+//TODO Create parser of ASCII files
+//TODO Check if keywords are strictly correct
+//input point's to a place after the header
 std::vector<TriangleStl> *Parser::parseAscii(char *input, uint64_t size) {
+    input = omitWhiteSpaces(input, size);
     return nullptr;
 }
-
+//TODO Create parser of binary files
+//input point's to a place after the header
 std::vector<TriangleStl> *Parser::parseBin(char *input, uint64_t size) {
     return nullptr;
 }
-
-//TODO Create parser of binary files
-//TODO Create parser of ASCII files
--- src/parserStl.h
+++ src/parserStl.h
@@ -5,8 +5,9 @@
 #include <fstream>
 class Parser{
 private:
-    std::vector<TriangleStl>* parseBin( char input[], uint64_t  size);
-    std::vector<TriangleStl>* parseAscii( char input[], uint64_t  size);
+    static char* omitWhiteSpaces(char * input, uint64_t size);
+    static std::vector<TriangleStl>* parseBin( char input[], uint64_t  size);
+    static std::vector<TriangleStl>* parseAscii( char input[], uint64_t  size);
 public:
     static std::vector<TriangleStl>* parseFile( char input[], uint64_t  size);
 
