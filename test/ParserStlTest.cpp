624ba78de2ebd19fd1c2a267d758e21483ed17bf
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-24 20:22:37 +0200

    Renaming ParserStl -> ParserStlTest in test directory

--- test/ParserStlTest.cpp
+++ test/ParserStlTest.cpp
@@ -1,7 +1,10 @@
 #include "Catch/catch.hpp"
+#include <iosfwd>
+#include <parserStl.h>
 
 TEST_CASE("Check if ParserStl parses ASCI file and returns correct vector of Traingles", "[ParserStl][parseFile][ascii]"){
-
+    auto ifstream = new std::ifstream("./example.stl", std::ifstream::in | std::ifstream::binary);
+//    auto parser = new Parser
 }
 
 TEST_CASE("Check if ParserStl parses bin file and returns correct vector of Traingles", "[ParserStl][parseFile][bin]"){
