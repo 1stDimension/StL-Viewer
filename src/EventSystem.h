d962756f0a79627beafd617c8472ebeb53ef1f85
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-28 14:55:54 +0200

    Renaming functions in EventSystem

--- src/EventSystem.h
+++ src/EventSystem.h
@@ -6,6 +6,8 @@
 class EventSystem {
     GLFWwindow* wwindow;
 //    TODO Consider adding multi threading
+//    friend void mouseButtonEvents(GLFWwindow *window, int button, int action, int mods);
+//    friend void keyEvents(GLFWwindow *window, int key, int scancode, int action, int mods);
 public:
     EventSystem(GLFWwindow *wwindow);
 
