d922ab9aa35250360cb4e43efed6fa4aa81da052
1stDimension <23144389+1stdimension@users.noreply.github.com>
2019-05-28 14:48:44 +0200

    Renaming functions in EventSystem

--- src/EventSystem.cpp
+++ src/EventSystem.cpp
@@ -8,29 +8,19 @@ void EventSystem::proces() {
     glfwPollEvents();
 }
 
-void arrowEvents(GLFWwindow *window, int key, int scancode, int action, int mods) {
-//    GLFW_KEY_RIGHT    262
-//    GLFW_KEY_LEFT    263
-//    GLFW_KEY_DOWN    264
-//    GLFW_KEY_UP     265
-    if (key >= GLFW_KEY_RIGHT && key <= GLFW_KEY_UP && action == GLFW_PRESS){
-        std::cout<< "\t"<< key <<std::endl;
-    }
-}
-
-void mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
+void mouseButtonEvents(GLFWwindow *window, int button, int action, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
         std::cout << "\t" "GLFW_MOUSE_BUTTON_LEFT + GLFW_PRESS" << std::endl;
     }
 }
 
-void shift_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+void keyEvents(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if ((key == GLFW_KEY_LEFT_SHIFT || key == GLFW_KEY_RIGHT_SHIFT) && (action == GLFW_PRESS))
         std::cout << "\t" "Shift" << std::endl;
 }
 
 void EventSystem::setup() {
-//    keyFunctions[0]  = shift_callback;
-    glfwSetKeyCallback(this->wwindow, shift_callback);
-    glfwSetMouseButtonCallback(this->wwindow, mouse_button_callback);
+//    keyFunctions[0]  = keyEvents;
+    glfwSetKeyCallback(this->wwindow, keyEvents);
+    glfwSetMouseButtonCallback(this->wwindow, mouseButtonEvents);
 }
